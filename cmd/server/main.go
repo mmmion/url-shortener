c5fada9b2b65928138c0a7009481bc3aa8838423
mion <lorenzo.dileone08@gmail.com>
2026-03-12 21:21:28 +0100

    add: custom server address and port and gitignore

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"url_shortner/internal/http"
 )
 
 func main() {
-	server, err := http.NewServer("127.0.0.1", 8080)
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Address (Default: localhost): ")
+	address, _ := reader.ReadString('\n')
+	address = strings.TrimSpace(address)
+	if address == "" {
+		address = "localhost"
+	}
+
+	fmt.Print("Port (Default: 8080): ")
+	portStr, _ := reader.ReadString('\n')
+	portStr = strings.TrimSpace(portStr)
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil || port <= 0 {
+		port = 8080
+	}
+
+	server, err := http.NewServer(address, port)
 	if err != nil {
 		return
 	}
